e576fd209674b9a42ecfee63adfe440eedd95d99
Marcus-Nastasi <vinnie.nsts@gmail.com>
2025-02-18 03:04:17 -0300

    fix: fixing login component.

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -5,6 +5,9 @@ import { BrnSelectImports } from '@spartan-ng/brain/select';
 import { HlmSelectImports } from '@spartan-ng/ui-select-helm';
 import { FormsModule } from '@angular/forms';
 import { ButtonSignInComponent } from "../../components/gmail/button-sign-in/button-sign-in.component";
+import { HlmInputDirective } from '@spartan-ng/ui-input-helm';
+import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
+import { HlmLabelDirective } from '@spartan-ng/ui-label-helm';
 
 /**
  * 
@@ -22,7 +25,10 @@ import { ButtonSignInComponent } from "../../components/gmail/button-sign-in/but
     BrnSelectImports,
     HlmSelectImports,
     FormsModule,
-    ButtonSignInComponent
+    ButtonSignInComponent,
+    HlmInputDirective,
+    HlmButtonDirective,
+    HlmLabelDirective
   ],
   template: `
     <main class="w-screen">
@@ -31,15 +37,55 @@ import { ButtonSignInComponent } from "../../components/gmail/button-sign-in/but
       </div>
       <form>
         <div class="flex justify-center">
-          <section hlmCard class="flex justify-center p-5 sm:w-fit md:w-1/2 xl:w-1/3">
+          <section hlmCard class="flex flex-col justify-center p-5 sm:w-fit md:w-1/2 xl:w-1/3">
+            <div class="flex flex-col">
+              <div class="mb-3">
+                <label hlmLabel >
+                  Name
+                  <input hlmInput type="text" placeholder="Name" class="w-full input-outline" >
+                </label>
+              </div>
+              
+              <div class="mb-3">
+                <label hlmLabel>
+                  E-mail
+                  <input hlmInput type="text" placeholder="E-mail" class="w-full input-outline" >
+                </label>
+              </div>
+
+              <div class="mb-3">
+                <label hlmLabel>
+                  Cel.
+                  <input hlmInput type="text" placeholder="Cel" class="w-full input-outline" >
+                </label>
+              </div>
+              
+              <div class="mb-3">
+                <label hlmLabel>
+                  Postal code
+                  <input hlmInput type="text" placeholder="Postal code" class="w-full input-outline" >
+                </label>
+              </div>
+            </div>
+
             <div class="m-3 flex items-center justify-between">
+              <button hlmBtn size="sm" >
+                Sign in
+              </button>
               <app-button-sign-in (click)="goToUrl($event)"  />
             </div>
+          
           </section>
         </div>
       </form>
     </main>
   `,
+  styles: `
+    .input-outline:focus {
+      border: 0.1px solid rgb(200, 200, 200);
+      box-shadow: 0px 0px 1.7px 0.01px;
+    }
+  `
 })
 export class LoginComponent {
 
